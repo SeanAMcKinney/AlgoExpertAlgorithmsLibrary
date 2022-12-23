43bb00f5318c32b3206468572b760369579da309
Sean McKinney <seanamckinney8@gmail.com>
2022-12-23 11:28:01 -0600

    Started in on the productsumroutine using the productsumalgorithm.  Need to look at how to serialize a userinput into json and add it to a list<object> for the algorithm to work.  This could be the way on another of the algorithms I'm working on.

--- ProductSumAlgorithm.cs
+++ ProductSumAlgorithm.cs
@@ -2,32 +2,32 @@
 
 namespace AlgoExpertAlgorithmsLibrary
 {
-    public class ProductSumProgram
+    public class ProductSumAlgorithm
+    {
+        // takes a "special" array and returns it's product sum. 
+        /* sample: { "array": [5, 2, [7, -1], 3, [6, [-13, 8], 4]] }  output: 12 */
+        // Calculated as: 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
+
+        public static int ProductSum(List<object> array)
         {
-            // takes a "special" array and returns it's product sum. 
-            /* sample: { "array": [5, 2, [7, -1], 3, [6, [-13, 8], 4]] }  output: 12 */
-            // Calculated as: 5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4)
-
-            public static int ProductSum(List<object> array)
-            {
-                return productSumHelper(array, 1);
-            }
-
-            public static int productSumHelper(List<object> array, int multiplier = 1)
-            {
-                int sum = 0;
-                foreach (object el in array)
-                {
-                    if (el is IList<object>)
-                    {
-                        sum += productSumHelper((List<object>)el, multiplier + 1);
-                    }
-                    else
-                    {
-                        sum += (int)el;
-                    }
-                }
-                return sum * multiplier;
-            }
+            return productSumHelper(array, 1);
         }
+
+        public static int productSumHelper(List<object> array, int multiplier = 1)
+        {
+            int sum = 0;
+            foreach (object el in array)
+            {
+                if (el is IList<object>)
+                {
+                    sum += productSumHelper((List<object>)el, multiplier + 1);
+                }
+                else
+                {
+                    sum += (int)el;
+                }
+            }
+            return sum * multiplier;
+        }
+    }
 }
